78e1f3b235fb25d3b69f668a5fc0f0bacb0fadec
melisa176 <carolinacastillon27@gmail.com>
2024-09-20 21:40:49 -0500

    realizar 3

---
+++ ejercicio3/ejercicio3.js
@@ -0,0 +1,7 @@
+function calcularIVA() {
+    const importe = parseFloat(document.getElementById('importe').value);
+    const tipoIVA = parseFloat(document.getElementById('iva').value);
+    const totalConIVA = importe + (importe * tipoIVA / 100);
+
+    document.getElementById('resultado').innerText = `Total con IVA incluido: €${totalConIVA.toFixed(2)}`;
+}
